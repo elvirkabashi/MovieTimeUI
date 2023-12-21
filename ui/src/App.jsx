33552c14048bbd84e2a1ebd7d1d7160c73f85cb0
elvirkabashi <74553516+elvirkabashi@users.noreply.github.com>
2023-12-21 22:37:20 +0100

    Login & SignUp

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
-import OnBoard from './pages/OnBoardPages/OnBoard'
+import LogIn from './pages/OnBoardPages/LogIn'
+import SingUp from './pages/OnBoardPages/SignUp';
+
 
 function App() {
 
   return (
     <>
-     <OnBoard/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LogIn />}/>
+        <Route path="/signup" element={<SingUp />}/>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
---
+++ ui/src/pages/OnBoardPages/SignUp.jsx
@@ -0,0 +1,94 @@
+import Ellipse from '../../components/Ellipse'
+import Footer from '../../components/Footer'
+import './assets/css/style.css'
+function SingUp() {
+
+  return (
+    <>
+        <div className='container' style={{backgroundColor: '#212121'}}>
+
+            <nav>
+                <img src={'logo.png'} alt="Logo" />
+            </nav>
+             <Ellipse top={100}/>
+             <div className="desktop d-lg-flex justify-content-center align-items-center " style={{height: '100vh'}}>
+           
+           <div className='me-5 p-5' style={{borderRight: '2px solid #a63910',width:'35%'}}>
+               <p className='text-white text-center py-2 fs-4'>Sign Up</p>
+               <form>
+                    <div className="">
+                        <label className="form-label text-white">Name</label>
+                        <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Your name'/>
+                    </div>
+                    <div className="">
+                        <label className="form-label text-white">Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Your email'/>
+                    </div>
+                    <div className="">
+                        <label className="form-label text-white">Password</label>
+                        <input type="password" className="form-control " id="exampleInputPassword1" placeholder="Password"/>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label text-white">Confitm Password</label>
+                        <input type="password" className="form-control " id="exampleInputPassword1" placeholder="Confirm Password"/>
+                    </div>
+
+                   <button type="submit" className="btn custom-btn-outline-primary">Sign Up</button>
+               </form>
+           </div>
+
+           <div className=''>
+                    <h3 className='text-white'>If you have an account
+                        <br />
+                        Log In!
+                    </h3>
+               <a href='/' type="submit" className="btn custom-btn-outline-primary my-3">LogIn</a>
+           </div>
+
+       </div>
+
+            {/*mobile */ }
+
+            <div className="mobile justify-content-center align-items-center flex-column" style={{height: '100vh'}}>
+           
+           <div className='m-3 p-3' style={{borderBottom: '2px solid #a63910',minWidth:'300px'}}>
+               <p className='text-white text-center py-2 fs-4'>Sign Up</p>
+               <form>
+                    <div className="">
+                        <label className="form-label text-white">Name</label>
+                        <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Your name'/>
+                    </div>
+                    <div className="">
+                        <label className="form-label text-white">Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Your email'/>
+                    </div>
+                    <div className="">
+                        <label className="form-label text-white">Password</label>
+                        <input type="password" className="form-control " id="exampleInputPassword1" placeholder="Password"/>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label text-white">Confitm Password</label>
+                        <input type="password" className="form-control " id="exampleInputPassword1" placeholder="Confirm Password"/>
+                    </div>
+
+                   <button type="submit" className="btn custom-btn-outline-primary">Sign Up</button>
+               </form>
+           </div>
+
+           <div className=''>
+                    <h3 className='text-white'>If you have an account
+                        <br />
+                        Log In!
+                    </h3>
+               <a href='/' type="submit" className="btn custom-btn-outline-primary my-3">LogIn</a>
+           </div>
+
+       </div>
+            
+        </div>
+        <Footer/>
+    </>
+  )
+}
+
+export default SingUp
