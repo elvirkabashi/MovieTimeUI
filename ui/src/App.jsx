de0c3a5ede4a021cb2ebe9a204633c0adb54f6af
elvirkabashi <74553516+elvirkabashi@users.noreply.github.com>
2023-12-22 16:01:23 +0100

    fix

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,13 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import OnBoard from './pages/OnBoardPages/OnBoard'
+import LogIn from './pages/OnBoardPages/LogIn'
+import SignUp from './pages/OnBoardPages/SignUp'
 
 function App() {
 
   return (
-    <>
-     <OnBoard/>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<OnBoard />}></Route>
+      <Route path="/login" element={<LogIn />}></Route>
+      <Route path="/singup" element={<SignUp />}></Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
