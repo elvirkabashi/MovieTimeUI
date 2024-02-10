3edbd54a3a5a0971d95b3455feb1e195e00667de
elvirkabashi <74553516+elvirkabashi@users.noreply.github.com>
2024-02-10 16:30:02 +0100

    Alert fix

--- ui/src/pages/OnBoardPages/LogIn.jsx
+++ ui/src/pages/OnBoardPages/LogIn.jsx
@@ -1,136 +1,137 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Ellipse from '../../components/Ellipse'
 import Logo from '../../components/Logo'
 import Cookies from 'js-cookie';
 import axios from 'axios';
 import './assets/css/style.css'
+
 function LogIn() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [error,setError] = useState()
-
+    const [error, setError] = useState();
+    const [successMessage, setSuccessMessage] = useState('');
 
     const handleLogin = async (e) => {
         e.preventDefault();
-    
         try {
             const response = await axios.post('http://localhost:7147/api/authentication/login', {
                 Email: email,
                 Password: password,
             });
-    
-            Cookies.set('authToken', response.data.token, { expires: 7 }); 
-    
+            Cookies.set('authToken', response.data.token, { expires: 7 });
             window.location.href = '/';
         } catch (error) {
-
             if (error.response && error.response.data) {
                 const { errors } = error.response.data;
-    
-
-                setError(errors)
-
+                setError(errors);
             } else {
-
                 console.error('Login failed. An unexpected error occurred:', error.message);
             }
         }
     };
-    
-    
 
-  return (
-    <>
-        <div className='container' style={{backgroundColor: '#212121'}}>
+    useEffect(() => {
+        const queryParams = new URLSearchParams(window.location.search);
+        if (queryParams.get('registered') === 'success') {
+            setSuccessMessage('You have been registered successfully!');
+        }
+    }, []);
 
-            <nav className='py-3'>
-                <Logo/>
-            </nav>
-             <Ellipse top={100}/>
-            <div className="desktop d-lg-flex justify-content-center align-items-center " style={{height: '100vh'}}>
-           
-                <div className='me-5 p-5' style={{borderRight: '2px solid #a63910',width:'35%'}}>
-                    <p className='text-white py-2 fs-4'>Log in to your account</p>
-                    <p className='text-danger'>{error}</p>
-                    <form onSubmit={handleLogin}>
-                        <div className="">
-                        <label className="form-label text-white">Email</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder='Your Username'
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                        </div>
-                        <div className="">
-                        <label className="form-label text-white">Password</label>
-                        <input
-                            type="password"
-                            className="form-control "
-                            id="exampleInputPassword1"
-                            placeholder="Password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                        </div>
-                        <div className="mb-3 form-check">
-                        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                        <label className="form-check-label text-white">Remember me</label>
-                        </div>
-                        <button type="submit" className="btn custom-btn-outline-primary">
-                        LogIn
-                        </button>
-                    </form>
+    return (
+        <>
+            <div className='container' style={{ backgroundColor: '#212121' }}>
+
+                <nav className='py-3'>
+                    <Logo />
+                </nav>
+                <Ellipse top={100} />
+                <div className="desktop d-lg-flex justify-content-center align-items-center " style={{ height: '100vh' }}>
+
+                    <div className='me-5 p-5' style={{ borderRight: '2px solid #a63910', width: '35%' }}>
+                        {successMessage && <p className="text-success" >
+                            {successMessage}
+                        </p>}
+                        <p className='text-white py-2 fs-4'>Log in to your account</p>
+                        <p className='text-danger'>{error}</p>
+                        <form onSubmit={handleLogin}>
+                            <div className="">
+                                <label className="form-label text-white">Email</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder='Enter your email' required
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </div>
+                            <div className="">
+                                <label className="form-label text-white">Password</label>
+                                <input
+                                    type="password"
+                                    className="form-control "
+                                    id="exampleInputPassword1"
+                                    placeholder='Enter password' required
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </div>
+                            <div className="mb-3 form-check">
+                                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                                <label className="form-check-label text-white">Remember me</label>
+                            </div>
+                            <button type="submit" className="btn custom-btn-outline-primary">
+                                LogIn
+                            </button>
+                        </form>
+                    </div>
+
+                    <div className='ms-5'>
+                        <h3 className='text-white'>Dont have an account ?
+                            <br />
+                            Create one.
+                        </h3>
+                        <p className='text-white'>Create an account and start your journey !</p>
+                        <a href='/signup' className='btn custom-btn-outline-primary'>Sign up</a>
+                    </div>
+
                 </div>
 
-                <div className='ms-5'>
-                    <h3 className='text-white'>Dont have an account ?
-                        <br />
-                        Create one.
-                    </h3>
-                    <p className='text-white'>Create an account and start your journey !</p>
-                    <a href='/signup' className='btn custom-btn-outline-primary'>Sign up</a>
+                {/* Mobile */}
+                <div className="mobile justify-content-center align-items-center flex-column" style={{ height: '100vh' }}>
+
+                    <div className='m-3 p-3' style={{ borderBottom: '2px solid #a63910', minWidth: '300px' }}>
+                        <p className='text-white py-2 fs-4'>Log in to your account</p>
+                        <form>
+                            <div className="">
+                                <label className="form-label text-white">Email address</label>
+                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Enter your email' required />
+                            </div>
+                            <div className="">
+                                <label className="form-label text-white">Password</label>
+                                <input type="password" className="form-control " id="exampleInputPassword1" placeholder="Enter password" required />
+                            </div>
+                            <div className="mb-3 form-check">
+                                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                                <label className="form-check-label text-white">Remember me</label>
+                            </div>
+                            <button type="submit" className="btn custom-btn-outline-primary">LogIn</button>
+                        </form>
+                    </div>
+
+                    <div className=''>
+                        <h3 className='text-white'>Dont have an account ?
+                            <br />
+                            Create one.
+                        </h3>
+                        <p className='text-white'>Create an account and start your journey !</p>
+                        <a href='/signup' className='btn custom-btn-outline-primary'>Sign up</a>
+                    </div>
+
                 </div>
 
             </div>
-
-            {/* Mobile */}
-            <div className="mobile justify-content-center align-items-center flex-column" style={{height: '100vh'}}>
-           
-           <div className='m-3 p-3' style={{borderBottom: '2px solid #a63910',minWidth:'300px'}}>
-               <p className='text-white py-2 fs-4'>Log in to your account</p>
-               <form>
-                   <div className="">
-                       <label className="form-label text-white">Email address</label>
-                       <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Your email'/>
-                   </div>
-                   <div className="">
-                       <label className="form-label text-white">Password</label>
-                       <input type="password" className="form-control " id="exampleInputPassword1" placeholder="Password"/>
-                   </div>
-                   <div className="mb-3 form-check">
-                       <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                       <label className="form-check-label text-white">Remember me</label>
-                   </div>
-                   <button type="submit" className="btn custom-btn-outline-primary">LogIn</button>
-               </form>
-           </div>
-
-           <div className=''>
-               <h3 className='text-white'>Dont have an account ?
-                   <br />
-                   Create one.
-               </h3>
-               <p className='text-white'>Create an account and start your journey !</p>
-               <a href='/signup' className='btn custom-btn-outline-primary'>Sign up</a>
-           </div>
-
-       </div>
-            
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
-export default LogIn
+export default LogIn;
--- ui/src/pages/OnBoardPages/SignUp.jsx
+++ ui/src/pages/OnBoardPages/SignUp.jsx
@@ -1,168 +1,155 @@
 import { useState } from 'react';
 import Ellipse from '../../components/Ellipse'
-import axios from 'axios';
 import Logo from '../../components/Logo'
+import axios from 'axios';
 import './assets/css/style.css'
+
 function SingUp() {
-
-    const [error,setError] = useState()
-
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState(false); 
 
     const [formData, setFormData] = useState({
-        name: '',
+        fullname: '',
         email: '',
         password: '',
-      });
-    
-      const handleChange = (e) => {
+    });
+
+    const handleChange = (e) => {
         setFormData({
-          ...formData,
-          [e.target.name]: e.target.value,
+            ...formData,
+            [e.target.name]: e.target.value,
         });
-      };
-    
-      const handleSubmit = async (e) => {
+    };
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
-      
-        const jsonData = {
-          Name: formData.name,
-          Email: formData.email,
-          Password: formData.password
-        };
+        setLoading(true);
+        try {
+            const response = await axios.post('http://localhost:7147/api/authentication/register', {
+                FullName: formData.fullname,
+                Email: formData.email,
+                Password: formData.password
+            });
+            setLoading(false);
+            if (!response.data.error) {
+                window.location.href = '/login?registered=success';
+            } else {
+                setError(response.data.errors);
+            }
+        } catch (error) {
+            setLoading(false);
+            console.error('Signup failed. An unexpected error occurred:', error.message);
+        }
+    };
 
-      
-          
-          const response = await axios.post('http://localhost:7147/api/authentication/register', jsonData);
-      
-          if (!response.data.error) {
-              window.location.href = '/login';
-          } else {
-              setError(response.data.errors);
-              console.log(response.data.errors)
-          }
-          
-        
-      
-        
-      };
+    return (
+        <>
+            <div className='container' style={{ backgroundColor: '#212121' }}>
 
-  return (
-    <>
-        <div className='container' style={{backgroundColor: '#212121'}}>
+                <nav className='py-3'>
+                    <Logo />
+                </nav>
+                <Ellipse top={100} />
+                <div className="desktop d-lg-flex justify-content-center align-items-center " style={{ height: '100vh' }}>
 
-            <nav className='py-3'>
-                <Logo/>
-            </nav>
-             <Ellipse top={100}/>
-             <div className="desktop d-lg-flex justify-content-center align-items-center " style={{height: '100vh'}}>
-           
-           <div className='me-5 p-5' style={{borderRight: '2px solid #a63910',width:'35%'}}>
-               <p className='text-white text-center py-2 fs-4'>Sign Up</p>
-               <p className='text-danger'>{error}</p>
-               <form onSubmit={handleSubmit}>
-                    <div className="">
-                        <label className="form-label text-white">Name</label>
-                        <input
-                            type='text'
-                            className='form-control'
-                            name='name'
-                            value={formData.name}
-                            onChange={handleChange}
-                            placeholder='Your name'
-                            required
-                        />
-                    </div>
-                    <div className="">
-                        <label className="form-label text-white">Email address</label>
-                        <input
-                            type='email'
-                            className='form-control'
-                            name='email'
-                            value={formData.email}
-                            onChange={handleChange}
-                            placeholder='Your email'
-                            required
-                        />
-                       </div>
-                    <div className="mb-3">
-                        <label className="form-label text-white">Password</label>
-                        <input
-                            type='password'
-                            className='form-control '
-                            name='password'
-                            value={formData.password}
-                            onChange={handleChange}
-                            placeholder='Password'
-                            required
-                        />
-                      </div>
-                      {/* <div className="mb-3">
-                        <label className="form-label text-white">Confirm Password</label>
-                        <input
-                            type='password'
-                            className='form-control '
-                            name='password'
-                            placeholder='Confirm Password'
-                            required
-                        />
-                      </div> */}
-                   
-
-                   <button type="submit" className="btn custom-btn-outline-primary">Sign Up</button>
-               </form>
-           </div>
-
-           <div className=''>
-                    <h3 className='text-white'>If you have an account
-                        <br />
-                        Log In!
-                    </h3>
-               <a href='/login' type="submit" className="btn custom-btn-outline-primary my-3">LogIn</a>
-           </div>
-
-       </div>
-
-            {/*mobile */ }
-
-            <div className="mobile justify-content-center align-items-center flex-column" style={{height: '100vh'}}>
-           
-           <div className='m-3 p-3' style={{borderBottom: '2px solid #a63910',minWidth:'300px'}}>
-               <p className='text-white text-center py-2 fs-4'>Sign Up</p>
-               <form>
-                    <div className="">
-                        <label className="form-label text-white">Name</label>
-                        <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Your name'/>
-                    </div>
-                    <div className="">
-                        <label className="form-label text-white">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Your email'/>
-                    </div>
-                    <div className="">
-                        <label className="form-label text-white">Password</label>
-                        <input type="password" className="form-control " id="exampleInputPassword1" placeholder="Password"/>
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label text-white">Confitm Password</label>
-                        <input type="password" className="form-control " id="exampleInputPassword1" placeholder="Confirm Password"/>
+                    <div className='me-5 p-5' style={{ borderRight: '2px solid #a63910', width: '35%' }}>
+                        <p className='text-white text-center py-2 fs-4'>Sign Up</p>
+                        <p className='text-danger'>{error}</p>
+                        <form onSubmit={handleSubmit}>
+                            <div className="">
+                                <label className="form-label text-white">Name</label>
+                                <input
+                                    type='text'
+                                    className='form-control'
+                                    name='fullname'
+                                    value={formData.name}
+                                    onChange={handleChange}
+                                    placeholder='Your name'
+                                    required
+                                />
+                            </div>
+                            <div className="">
+                                <label className="form-label text-white">Email address</label>
+                                <input
+                                    type='email'
+                                    className='form-control'
+                                    name='email'
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                    placeholder='Your email'
+                                    required
+                                />
+                            </div>
+                            <div className="mb-3">
+                                <label className="form-label text-white">Password</label>
+                                <input
+                                    type='password'
+                                    className='form-control '
+                                    name='password'
+                                    value={formData.password}
+                                    onChange={handleChange}
+                                    placeholder='Password'
+                                    required
+                                />
+                            </div>
+                            {!loading && <button type="submit" className="btn custom-btn-outline-primary">Sign Up</button>}
+                            {loading && <div className="spinner-border text-primary" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </div>}
+                        </form>
                     </div>
 
-                   <button type="submit" className="btn custom-btn-outline-primary">Sign Up</button>
-               </form>
-           </div>
+                    <div className=''>
+                        <h3 className='text-white'>If you have an account
+                            <br />
+                            Log In!
+                        </h3>
+                        <a href='/login' type="submit" className="btn custom-btn-outline-primary my-3">LogIn</a>
+                    </div>
 
-           <div className=''>
-                    <h3 className='text-white'>If you have an account
-                        <br />
-                        Log In!
-                    </h3>
-               <a href='/login' type="submit" className="btn custom-btn-outline-primary my-3">LogIn</a>
-           </div>
+                </div>
 
-       </div>
-            
-        </div>
-    </>
-  )
+                {/* mobile */}
+
+                <div className="mobile justify-content-center align-items-center flex-column" style={{ height: '100vh' }}>
+
+                    <div className='m-3 p-3' style={{ borderBottom: '2px solid #a63910', minWidth: '300px' }}>
+                        <p className='text-white text-center py-2 fs-4'>Sign Up</p>
+                        <form>
+                            <div className="">
+                                <label className="form-label text-white">Name</label>
+                                <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Your name' />
+                            </div>
+                            <div className="">
+                                <label className="form-label text-white">Email address</label>
+                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Your email' />
+                            </div>
+                            <div className="">
+                                <label className="form-label text-white">Password</label>
+                                <input type="password" className="form-control " id="exampleInputPassword1" placeholder="Password" />
+                            </div>
+                            <div className="mb-3">
+                                <label className="form-label text-white">Confitm Password</label>
+                                <input type="password" className="form-control " id="exampleInputPassword1" placeholder="Confirm Password" />
+                            </div>
+
+                            <button type="submit" className="btn custom-btn-outline-primary">Sign Up</button>
+                        </form>
+                    </div>
+
+                    <div className=''>
+                        <h3 className='text-white'>If you have an account
+                            <br />
+                            Log In!
+                        </h3>
+                        <a href='/login' type="submit" className="btn custom-btn-outline-primary my-3">LogIn</a>
+                    </div>
+
+                </div>
+
+            </div>
+        </>
+    )
 }
 
-export default SingUp
+export default SingUp;
